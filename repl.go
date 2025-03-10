18d5d68f58a7f1c9ac06220ff1e647b7fd1ba259
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2025-03-10 13:27:49 +0800

    add command functionality (exit, help)

--- repl.go
+++ repl.go
@@ -7,7 +7,26 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func initializeRepl() {
+	cliCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+	cliCommands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp(cliCommands),
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -17,9 +36,17 @@ func initializeRepl() {
 		cleaned := cleanInput(input)
 
 		if len(cleaned) > 0 {
-			fmt.Println("Your command was:", cleaned[0])
+			commandName := cleaned[0]
+			if command, exists := cliCommands[commandName]; exists {
+				err := command.callback()
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
 		} else {
-			fmt.Println("No command entered.")
+			fmt.Println("No command entered")
 		}
 	}
 }
@@ -28,4 +55,22 @@ func cleanInput(text string) []string {
 	fixed_str := strings.Fields(strings.ToLower(text))
 
 	return fixed_str
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(commands map[string]cliCommand) func() error {
+	return func() error {
+		fmt.Println("Welcome to the pokedex!")
+		fmt.Println("Usage:")
+		fmt.Println()
+		for _, cmd := range commands {
+			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+		}
+		return nil
+	}
 }
