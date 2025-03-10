e1f68653091be635acd94431aebe3251a9872ded
Nicholas Tankiang <nicholastankiang@Nicholass-MacBook-Pro.local>
2025-03-10 12:26:11 +0800

    add REPL function

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned := cleanInput(input)
+		fmt.Println("Your command was:", cleaned[0])
+	}
 }
 
 func cleanInput(text string) []string {
